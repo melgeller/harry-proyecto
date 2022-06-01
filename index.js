437963d7e71db670be63cec0054ed9c4716e0612
melgeller <melgeller.foto@gmail.com>
2022-06-01 10:33:16 +0200

    no se

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const logger = require("morgan");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const server = express();
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+server.use(
+    cors({
+      origin: "*",
+      credentials: true,
+    })
+  );
+
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }));
+  
---
+++ src/utils/database.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const DB_URL = process.env.MONGO_DB;
+
+const connect = async () => {
+  try {
+    const DB = await mongoose.connect(DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const { name, host } = DB.connection;
+    console.log(`Connected to DB: ${name}, in host ${host}`);
+  } catch (error) {
+    console.error("Error connecting to DB", error);
+  }
+};
+
+module.exports = {connect}
