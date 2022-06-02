454505fe3bc547abb04bb0fbbf9b1d5b71c24137
dani molina <franciscomolinadiaz1@gmail.com>
2022-06-02 17:18:34 +0200

    personaje x cada casa

--- front/front.index.js
+++ front/front.index.js
@@ -3,6 +3,7 @@ const casas$$ = document.querySelector(".casas");
 const personaje$$ = document.querySelector(".personajes")
 
 
+
 const getCasas = async ()=> {
     const casasApi = await fetch("http://localhost:5000/casas");
     const casasJson = await casasApi.json();
@@ -22,20 +23,34 @@ const pintandoCasa = (arraycards) => {
         const tituloCasa$$ = document.createElement("h2");
         const escudo$$ = document.createElement("img");
         const animal$$ = document.createElement("p");
-
+        const divNuevo$$ = document.createElement("div")
+        
 
         tituloCasa$$.textContent = casa.name;
         escudo$$.src= casa.escudo;
         animal$$.textContent = casa.fundador;
+        
 
         divCasa$$.appendChild(tituloCasa$$);
         divCasa$$.appendChild(escudo$$);
         divCasa$$.appendChild(animal$$);
         casas$$.appendChild(divCasa$$);
+        casas$$.appendChild(divNuevo$$)
         escudo$$.className = "escudo";
         divCasa$$.className= "casita";
         animal$$.className = "animales";
         tituloCasa$$.className = "titulocasa";
+        divNuevo$$.className = "divNuevo";
+        console.log(casa);
+        for (const chaval of casa.personajes) {
+            const personajesCasa$$ = document.createElement("p");
+            personajesCasa$$.textContent = chaval.name;
+            divNuevo$$.appendChild(personajesCasa$$)
+
+            
+        }
+
+        
     }
 }
 
@@ -53,6 +68,7 @@ const pintandopPersonaje = (arraycards) => {
         const foto$$ = document.createElement("img");
         const casa$$ = document.createElement("p");
         const age$$ = document.createElement("p");
+        
 
         
 
@@ -76,3 +92,47 @@ const pintandopPersonaje = (arraycards) => {
     }
     }
 }
+
+const pintandopVarita = (arraycards) => {
+    for (const casa of arraycards.casas) {
+        console.log(casa);
+        for (const personaje of casa.personajes.varitas) {
+            
+
+            const divpersonaje$$ = document.createElement("div")
+            const titulopersonaje$$ = document.createElement("h2");
+            const foto$$ = document.createElement("img");
+            const casa$$ = document.createElement("p");
+            const age$$ = document.createElement("p");
+
+            
+
+
+            titulopersonaje$$.textContent = personaje.name;
+            foto$$.src= personaje.photo;
+            casa$$.textContent = "Casa: " + personaje.house;
+            age$$.textContent = "Edad: " + personaje.age;
+
+            divpersonaje$$.appendChild(titulopersonaje$$);
+            divpersonaje$$.appendChild(foto$$);
+            divpersonaje$$.appendChild(casa$$);
+            divpersonaje$$.appendChild(age$$);
+            personaje$$.appendChild(divpersonaje$$);
+
+            foto$$.className = "foto_personaje";
+            divpersonaje$$.className= "personajito";
+            casa$$.className = "casa_edad";
+            titulopersonaje$$.className = "titulo_personaje";
+            age$$.className = "casa_edad";
+                
+            }
+            
+        
+        
+    }
+    }
+
+
+
+
+    
